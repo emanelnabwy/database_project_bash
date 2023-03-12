c82163ad2c8a859d3182a804aa8d56e3fa994d76
Eman El nabwy <55757857+emanelnabwy@users.noreply.github.com>
2023-03-13 00:25:58 +0200

    Create printmsg.sh

---
+++ printmsg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###########print menu
+
+function PrintMainMenu {
+        echo "Main menu"
+        echo -e "\t a) Authenticate"
+        echo -e "\t 1) Add a customer"
+        echo -e "\t 2) Update a customer email"
+        echo -e "\t 3) Delete a customer"
+        echo -e "\t 4) Query a customer email"
+        echo -e "\t 5) Quit"
+        echo -n "Please, select a option: "
+}
+
